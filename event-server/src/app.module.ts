23463844e6cb82d48d713abd8ceba0e9517d3944
grr1203 <gyrms9412@naver.com>
2025-05-18 21:40:50 +0900

    [EventServer] init

---
+++ event-server/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/auth-db',
+      }),
+    }),
+  ],
+  controllers: [AppController]
+})
+export class AppModule {}
